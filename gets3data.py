54081fcd6fcb494cb724c8b07fa97bedcf1dfbbb
Coleman <coleman.broaddus@gmail.com>
2026-04-23 14:35:13 -0400

    parameterize mito_retrieval() by image and mito centerpoint.

--- gets3data.py
+++ gets3data.py
@@ -451,59 +451,60 @@ def f9(w, stride=4, patch_size=16):
         print(f"{name}  variance: {pca.explained_variance_ratio_}")
         w.add_image(pca_img, name=name, rgb=True)
 
-def f10(w, query_yx=(107, 317), bg_yx=(232, 230), stride=2):
-    """Task 2.3.1: Embedding-based retrieval using a query mito point.
-    Compute similarity to query mito minus similarity to background point."""
+def mito_retrieval(w, img, query_yx, stride=2, min_distance=10, threshold_rel=0.3, name=''):
+    """Cosine similarity retrieval from a query mito point.
+    img: 2D grayscale numpy array.
+    query_yx: (y, x) pixel location of query mitochondrion."""
+    from skimage.feature import peak_local_max
     patch_size = 16
+
     model = load_dino()
     model.patch_embed.proj.stride = (stride, stride)
 
-    a, b = 16*30, 16*60
-    ss = (2233//2, slice(a,b), slice(a,b))
-    x = loadN5('jrc_mus-liver', 'em/fibsem-uint8/s2/', ss)
-    H, W = (x.shape[0] // 16) * 16, (x.shape[1] // 16) * 16
-    x_crop = x[:H, :W]
+    H, W = (img.shape[0] // 16) * 16, (img.shape[1] // 16) * 16
+    x_crop = img[:H, :W].astype(np.float32)
 
-    y_full = run_dino(model, x_crop.astype(np.float32))
+    y_full = run_dino(model, x_crop)
     patch_tokens = y_full['x_norm_patchtokens'].squeeze(0)  # (N, 384)
     patch_normed = torch.nn.functional.normalize(patch_tokens, dim=-1)
 
     pH = (H - patch_size) // stride + 1
     pW = (W - patch_size) // stride + 1
 
-    def pixel_to_patch(yx):
-        y, x = yx
-        return min(y // stride, pH - 1), min(x // stride, pW - 1)
-
-    # Query mito embedding
-    qi, qj = pixel_to_patch(query_yx)
+    # Map query pixel to patch index
+    qy, qx = query_yx
+    qi = min(qy // stride, pH - 1)
+    qj = min(qx // stride, pW - 1)
     query_emb = torch.nn.functional.normalize(patch_tokens[qi * pW + qj].unsqueeze(0), dim=-1)
-    sim_mito = (patch_normed @ query_emb.T).squeeze(-1).numpy()
 
-    # Background embedding
-    bi, bj = pixel_to_patch(bg_yx)
-    bg_emb = torch.nn.functional.normalize(patch_tokens[bi * pW + bj].unsqueeze(0), dim=-1)
-    sim_bg = (patch_normed @ bg_emb.T).squeeze(-1).numpy()
+    # Cosine similarity
+    cos_sim = (patch_normed @ query_emb.T).squeeze(-1).numpy()
 
-    # Mito score = similarity to mito - similarity to background
-    score = sim_mito - sim_bg
+    sim_grid = cos_sim.reshape(pH, pW, 1)
+    sim_img = overlap_average(sim_grid, H, W, patch_size, stride).squeeze(-1)
 
-    # Visualize
-    score_grid = score.reshape(pH, pW, 1)
-    score_img = overlap_average(score_grid, H, W, patch_size, stride).squeeze(-1)
-
-    # Find local maxima on the combined score
-    from skimage.feature import peak_local_max
-    peaks = peak_local_max(score_img, min_distance=10, threshold_rel=0.3)
+    # Detect peaks
+    peaks = peak_local_max(sim_img, min_distance=min_distance, threshold_rel=threshold_rel)
     print(f"Found {len(peaks)} local maxima")
 
-    w.add_image(x_crop, name='original')
-    w.add_image(score_img, name='mito_score', colormap='inferno')
-    w.add_points(np.array([list(query_yx)]), name='query_mito', size=10, face_color='red')
-    w.add_points(np.array([list(bg_yx)]), name='query_bg', size=10, face_color='blue')
-    w.add_points(peaks, name='similar_peaks', size=8, face_color='green')
+    pfx = f'{name} ' if name else ''
+    w.add_image(x_crop, name=f'{pfx}original')
+    w.add_image(sim_img, name=f'{pfx}cosine_sim', colormap='inferno')
+    w.add_points(np.array([list(query_yx)]), name=f'{pfx}query', size=10, face_color='red')
+    w.add_points(peaks, name=f'{pfx}detections', size=8, face_color='green')
 
-    return score_img
+    return sim_img, peaks
+
+def f10test(w):
+    # Mouse liver
+    c,b,a = 12057, 12301, 6229
+    img1 = loadZarr('jrc_mus-liver', 'recon-1/em/fibsem-uint8/s2', p2patch(a,b,c, s=2, const=0))
+    mito_retrieval(w, img1, query_yx=(44, 163), name='liver')
+
+    # Mouse kidney
+    # c,b,a = 6417, 4150, 10157
+    # img2 = loadZarr('jrc_mus-kidney', 'recon-1/em/fibsem-uint8/s2', p2patch(a,b,c, s=2, const=0))
+    # mito_retrieval(w, img2, query_yx=(165, 250), name='kidney')
 
 def f11(w, sigma_range=(1, 3, 5, 9, 15)):
     """Blur + threshold segmentation sweep on mus-kidney image."""
